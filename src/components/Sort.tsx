2fa982f16a5e5ba45599b9922097344a514486e9
Denis Kravchuk <deniskrv@gmail.com>
2022-09-01 19:58:59 +0300

    типизировал функции и event`ы

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -7,6 +7,10 @@ type SortItem = {
   sortProperty: string;
 };
 
+type PopupClick = MouseEvent & {
+  path: Node[];
+};
+
 export const list: SortItem[] = [
   { name: 'популярности (DESC)', sortProperty: 'rating' },
   { name: 'популярности (ASC)', sortProperty: '-rating' },
@@ -29,8 +33,9 @@ const Sort = () => {
   };
 
   useEffect(() => {
-    const eventClickOutside = (event: any) => {
-      if (!event.path.includes(sortRef.current)) {
+    const eventClickOutside = (event: MouseEvent) => {
+      const _event = event as PopupClick;
+      if (sortRef.current && !_event.path.includes(sortRef.current)) {
         setIsVisible(false);
       }
     };
