0a48670d8d11b00081158d462a3f28c56e3e0f41
Denis Kravchuk <deniskrv@gmail.com>
2022-09-07 21:59:06 +0300

    убрал поиск со страницы корзины

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
             </div>
           </div>
         </Link>
-        <Search />
+        {pathname !== '/cart' && <Search />}
         <div className="header__cart">
           {pathname !== '/cart' && (
             <Link to="/cart" className="button button--cart">
