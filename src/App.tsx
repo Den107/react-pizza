b517c6b1067beaec8f8044e189a30841d28bf5c7
Denis Kravchuk <deniskrv@gmail.com>
2022-09-15 18:10:22 +0300

    настроил динамические импорты

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import './scss/app.scss';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-
 import { Routes, Route } from 'react-router-dom';
-import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
+import { lazy, Suspense } from 'react';
+
+const Cart = lazy(() => import(/* webpackChunkName: 'Cart'*/ './pages/Cart'));
+const FullPizza = lazy(() => import(/* webpackChunkName: 'FullPizza'*/ './pages/FullPizza'));
+const NotFound = lazy(() => import(/* webpackChunkName: 'NotFound'*/ './pages/NotFound'));
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<MainLayout />}>
         <Route path={''} element={<Home />} />
-        <Route path={'cart'} element={<Cart />} />
-        <Route path={'pizza/:id'} element={<FullPizza />} />
-        <Route path={'*'} element={<NotFound />} />
+        <Route
+          path={'cart'}
+          element={
+            <Suspense fallback={'Загрузка корзины...'}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path={'pizza/:id'}
+          element={
+            <Suspense fallback={'Загрузка пиццы...'}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path={'*'}
+          element={
+            <Suspense>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
